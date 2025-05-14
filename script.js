3e97a62fa71f2b3f87846617c1525fcf48fee00f
giesta_andini <giestaandiniputri@gmail.com>
2025-05-14 14:49:42 +0700

    new repositori 2

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const toggle = document.getElementById('theme-toggle');
 const body = document.body;
 
-// Cek preferensi sebelumnya di localStorage
 if (localStorage.getItem('theme') === 'dark') {
   body.classList.add('dark');
   toggle.textContent = '🌙';
 }
 
-// Toggle tema saat tombol diklik
 toggle.addEventListener('click', () => {
   body.classList.toggle('dark');
   const isDark = body.classList.contains('dark');
 
-  // Simpan pilihan ke localStorage
   localStorage.setItem('theme', isDark ? 'dark' : 'light');
 
   toggle.textContent = isDark ? '🌙' : '🌞';
@@ -35,7 +32,7 @@ function typeWriter() {
     setTimeout(typeWriter, 50);
   } else {
     isDeleting = !isDeleting;
-    setTimeout(typeWriter, isDeleting ? 2000 : 500); // jeda sebelum hapus & ketik ulang
+    setTimeout(typeWriter, isDeleting ? 2000 : 500); 
   }
 }
 
@@ -45,9 +42,7 @@ const navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
-    // hapus semua 'active'
     navLinks.forEach(nav => nav.classList.remove('active'));
-    // tambahkan ke yang diklik
     link.classList.add('active');
   });
 });
