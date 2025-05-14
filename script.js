9a582841994b21f6544bec3f162c25f75e2644e4
giesta_andini <giestaandiniputri@gmail.com>
2025-05-14 22:33:51 +0700

    new about

--- script.js
+++ script.js
@@ -15,29 +15,6 @@ toggle.addEventListener('click', () => {
   toggle.textContent = isDark ? '🌙' : '🌞';
 });
 
-const text = "Junior Web Programming";
-const typingText = document.getElementById("typing-text");
-
-let index = 0;
-let isDeleting = false;
-
-function typeWriter() {
-  if (!isDeleting && index <= text.length) {
-    typingText.textContent = text.substring(0, index);
-    index++;
-    setTimeout(typeWriter, 100);
-  } else if (isDeleting && index >= 0) {
-    typingText.textContent = text.substring(0, index);
-    index--;
-    setTimeout(typeWriter, 50);
-  } else {
-    isDeleting = !isDeleting;
-    setTimeout(typeWriter, isDeleting ? 2000 : 500); 
-  }
-}
-
-window.onload = typeWriter;
-
 const navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach(link => {
